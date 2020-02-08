b1f8688ed58a85c6021ff642f7f520771d6d8235
kip6163 <kip6163@rit.edu>
2020-02-08 14:32:35 -0500

    Added LaundryRoom and Machine classes

---
+++ src/LaundryRoom.java
@@ -0,0 +1,8 @@
+public class LaundryRoom {
+    private int NUM_WASHERS = 20;
+    private int NUM_DRYERS = 20;
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Machine.java
@@ -0,0 +1,9 @@
+public class Machine {
+    private boolean isWorking;
+    private boolean isInUse;
+
+    public Machine(boolean isWorking, boolean isInUse){
+        this.isWorking = isWorking;
+        this.isInUse = isInUse;
+    }
+}
