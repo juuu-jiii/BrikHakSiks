911a61c48462f0431f68b532a64877088e3f7061
kip6163 <kip6163@rit.edu>
2020-02-08 15:17:09 -0500

    Added methods to Washer and Dryer

--- src/Dryer.java
+++ src/Dryer.java
@@ -3,4 +3,24 @@ public class Dryer extends Machine{
     public Dryer(boolean isWorking, boolean isInUse) {
         super(isWorking, isInUse);
     }
+    @Override
+    public boolean checkIfInUse(){
+        if(isInUse == false){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    @Override
+    public boolean checkIfWorking(){
+        if(isWorking == false){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
 }
--- src/Washer.java
+++ src/Washer.java
@@ -3,4 +3,25 @@ public class Washer extends Machine {
     public Washer(boolean isWorking, boolean isInUse){
         super(isWorking, isInUse);
     }
+
+    @Override
+    public boolean checkIfInUse(){
+       if(isInUse == false){
+           return false;
+       }
+       else{
+           return true;
+       }
+    }
+
+    @Override
+    public boolean checkIfWorking(){
+        if(isWorking == false){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
 }
