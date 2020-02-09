f858cb92e440cea9b7168cd5cc61001d2a343fa8
juuu-jiii <juuu-jiii@users.noreply.github.com>
2020-02-08 20:45:33 -0500

    updated Dryer class

--- src/Dryer.java
+++ src/Dryer.java
@@ -1,7 +1,21 @@
 public class Dryer extends Machine{
 
-    public Dryer(boolean isWorking, boolean isAvailable) {
-        super(isWorking, isAvailable);
+    public Dryer(String type) {
+        super(type);
     }
 
+    @Override
+    public void operate() {
+        // Set internal timer to remind in 43 minutes
+        // Set internal timer to notify in 45 minutes
+        isAvailable = false;
+        System.out.println("Timer set!");
+    }
+
+    @Override
+    public void report() {
+        // Send message to RIT Housing
+        isWorking = false;
+        System.out.println("Report sent!");
+    }
 }
